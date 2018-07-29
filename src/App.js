2d5861f0c9e554679c45e759dd48e88326550435
BSP森下 <morishita@bestperson.jp>
2018-07-30 08:03:24 +0900

    learn porps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div><h1>Hello, world</h1></div>
-    );
-  }
+const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10
+    },
+    {
+      name: "Hanako",
+      age: 5
+    },
+    {
+      name: "NoName"
+    }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, i) => {
+          return <User key={i} name={profile.name} age={profile.age} />
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>{props.name}, and {props.age}</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
